ce39ae2b6128deed58df417f99a382bcda3cbecd
chenjunzhou <chenjunzhou@163.com>
2019-03-19 14:03:59 +0800

    测

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
             'Host'=>'www.amazon.co.jp',
             'accept-encoding'=>'gzip, deflate'
         ]
-    ])->getHtml();
-    echo ($res);
+    ])->find('#pd_pgh_B0782339QB_title')->texts();
+    print_r($res);
 //    return view('welcome');
 });
